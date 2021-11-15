15b9450f57af2eea982b65bb23d84a86a702da90
Chithang0210 <78576625+Duongchithang@users.noreply.github.com>
2021-11-15 12:05:22 +0700

    music-play

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [{
+        name: "Ghé qua",
+        artist: "Dofu",
+        img: "music-0",
+        src: "music-0"
+    },
+
+    {
+        name: "Người em cố đô",
+        artist: "Rum X Đaa",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Có hẹn với thanh xuân",
+        artist: "Monstar",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Trời Giấu Trời Mang Đi",
+        artist: "Amee x ViruSs",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Giấc Mơ Rất Thơ",
+        artist: "Mer",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Tình Thật Tình Mơ",
+        artist: "Mer",
+        img: "music-3",
+        src: "music-3"
+    },
+
+];
